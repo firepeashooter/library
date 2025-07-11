c597c955d2ff6b66d0a75ccab5de2c6a712b64c1
Ben Braun <benjamin.braun2003@gmail.com>
2025-07-11 13:26:26 -0400

    Add additional styling to dialogue

--- scripts/newBooks.js
+++ scripts/newBooks.js
@@ -4,9 +4,12 @@ const close = document.querySelector(".close");
 
 newBook.addEventListener("click", () => {
     dialogue.showModal();
+    dialogue.classList.add("show");
 });
 
 
 close.addEventListener("click", () => {
     dialogue.close();
+    dialogue.classList.remove("show");
+
 });
